beab7acceb869db4f4f46737ef482be784c62128
Phyks (Lucas Verney) <phyks@phyks.me>
2015-11-15 18:10:53 +0100

    Fix a 500 when no match was found in BASE

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import settings
 from bottle import route, redirect, request, run
 from oaipmh.client import Client
 from oaipmh.metadata import MetadataRegistry, base_dc_reader
+from oaipmh.error import NoRecordsMatchError
 
 
 @route("/<doi:path>")
@@ -11,15 +12,18 @@ def doi(doi):
     registry = MetadataRegistry()
     registry.registerReader('base_dc', base_dc_reader)
     client = Client(settings.oaipmh_url, registry)
-    for record in client.listRecords(metadataPrefix="base_dc",
-                                     set=("proaixy:doi:%s" % (doi,))):
-        try:
-            # If openaccess version available, redirect the user to it
-            has_oa = True in [i == "1" for i in record[1].getField("oa")]
-            if has_oa:
-                redirect(record[1].getField("link")[0])
-        except KeyError:
-            pass
+    try:
+        for record in client.listRecords(metadataPrefix="base_dc",
+                                         set=("proaixy:doi:%s" % (doi,))):
+            try:
+                # If openaccess version available, redirect the user to it
+                has_oa = True in [i == "1" for i in record[1].getField("oa")]
+                if has_oa:
+                    redirect(record[1].getField("link")[0])
+            except KeyError:
+                pass
+    except NoRecordsMatchError:
+        pass
     # If not found, pass it to upstream doi handler
     doi_upstream_url = "https://dx.doi.org/%s?" % (doi,)
     for k, v in request.query.decode().items():
